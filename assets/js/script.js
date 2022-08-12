cddc3356159a33a37898994fc32d5e5f1c248746
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-12 20:13:32 +0000

    add winner function with if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,53 @@ rockBtn.addEventListener("onclick", startGame());
 
 
 function startGame(){
-    return alert('Winner!');
+    const player = rockBtn;
+    const computer = options[Math.floor(Math.random()*options.length)];
+    winner();
+    
 };
 
 
 
+function winner(){
+    if (player === rock && computer === rock){
+        playerScore++;
+        computerScore++;
+        alert('Tie!');
+    } else if(player === rock && computer === paper) {
+        counter++;
+        computerScore++;
+        alert('Computer Wins!');
+    } else if (player === rock && computer === scissors){
+        playerScore++;
+        alert('Player Wins!');
+    }
+    
+    if (player === paper && computer === paper){
+        playerScore++;
+        computerScore++;
+        alert('Tie!');
+    } else if(player === paper && computer === scissors){
+        computerScore++;
+        alert('Computer Wins!');
+    } else if (player === paper && computer === rock){
+        playerScore++;
+        alert('Player Wins!');
+    }
+    
+    if (player === scissors && computer === scissors){
+        playerScore++;
+        computerScore++;
+        alert('Tie!');
+    } else if(player === scissors && computer === rock){
+        computerScore++;
+        alert('Computer Wins!');
+    } else if (player === scissors && computer === paper){
+        playerScore++;
+        alert('Player Wins!');
+    }
+    
 
 
-const player = rockBtn;
-const computer = options[Math.floor(Math.random()*options.length)];
+}
+
