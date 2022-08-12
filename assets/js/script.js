ecabd97a93194af3890cc63f76fde3f91eec5fd2
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-12 20:06:46 +0000

    removed all code and started again. new variables, listener for button click added but not working correct, computer variable with random rock paper or scissors gen added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,19 @@
 
-const buttons = document.getElementsByClassName('rockpaperscissors');
-const playerScore = document.getElementsByClassName('playerscore');
-const computerScore = document.getElementsByClassName('computerscore');
-const choices = ["rock", "paper", "scissors"];
-let movesleft = document.getElementsByClassName('movesleft');
-let counter = 0;
+const rockBtn = document.getElementsByClassName('rock');
+const paperBtn = document.getElementsByClassName('paper');
+const scissorsBtn = document.getElementsByClassName('Scissors');
+const options = ["rock","paper","scissors"];
+
+rockBtn.addEventListener("onclick", startGame()); 
+
+
+function startGame(){
+    return alert('Winner!');
+};
 
 
 
 
 
-if (player === rock && computer === rock){
-    counter++;
-    playerScore++;
-    computerScore++;
-    alert('Tie!');
-} else if(player === rock && computer === paper) {
-    counter++;
-    computerScore++;
-    alert('Computer Wins!');
-} else if (player === rock && computer === scissors){
-    counter++;
-    playerScore++;
-    alert('Player Wins!');
-}
-
-if (player === paper && computer === paper){
-    counter++;
-    playerScore++;
-    computerScore++;
-    alert('Tie!');
-} else if(player === paper && computer === scissors){
-    counter++;
-    computerScore++;
-    alert('Computer Wins!');
-} else if (player === paper && computer === rock){
-    counter++;
-    playerScore++;
-    alert('Player Wins!');
-}
-
-if (player === scissors && computer === scissors){
-    counter++;
-    playerScore++;
-    computerScore++;
-    alert('Tie!');
-} else if(player === scissors && computer === rock){
-    counter++;
-    computerScore++;
-    alert('Computer Wins!');
-} else if (player === scissors && computer === paper){
-    counter++;
-    playerScore++;
-    alert('Player Wins!');
-}
+const player = rockBtn;
+const computer = options[Math.floor(Math.random()*options.length)];
