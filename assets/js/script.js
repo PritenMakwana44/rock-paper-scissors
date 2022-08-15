ca660407fe82187db1d74a1047715d4f4c7a6398
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-15 16:42:08 +0000

    game working and tested via console log, player increment and computer increment working to keep score. changed variables in html to id for scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-const playerScore = document.getElementsByClassName("playerScore");
-const computerScore = document.getElementsByClassName("computerScore");
+
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+playerScore = 0;
+computerScore = 0;
+
+playerScore.innerHTML = "Player:" + playerScore;
+
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
@@ -8,7 +14,44 @@ const choices = ['rock','paper','scissors'];
 
 function game(player) {
     const computer = computerChoiceGen();
+    
+    if (player === 'rock' && computer === 'rock'){
+        alert('Tie!');
+        playerScore++;
+        computerScore++;
+    } else if(player === 'rock' && computer === 'paper') {
+        alert('Computer Wins!');
+        computerScore++;
+    } else if (player === 'rock' && computer === 'scissors'){
+        alert('Player Wins!');
+        playerScore++;
+    }
+    if (player === 'paper' && computer === 'paper'){
+        alert('Tie!');
+        playerScore++;
+        computerScore++;
+    } else if(player === 'paper' && computer === 'scissors'){
+        alert('Computer Wins!');
+        computerScore++;
+    } else if (player === 'paper' && computer === 'rock'){
+        alert('Player Wins!');
+        playerScore++;
+    }
+    if (player === 'scissors' && computer === 'scissors'){
+        alert('Tie!');
+        playerScore++;
+        computerScore++;
+    } else if(player === 'scissors' && computer === 'rock'){
+        alert('Computer Wins!');
+        computerScore++;
+    } else if (player === 'scissors' && computer === 'paper'){
+        alert('Player Wins!');
+        computerScore++;
+    }
+
     console.log(computer);
+    console.log(player);
+
 }
 
 function computerChoiceGen(){
