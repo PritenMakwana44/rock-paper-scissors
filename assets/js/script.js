f4c4d544587a65e35c035f5013445a8ffafdac4a
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-16 19:05:13 +0000

    fix spacing on all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,58 @@
-
 let playerScore = document.getElementById("playerScore");
 let computerScore = document.getElementById("computerScore");
 playerScore = 0;
 computerScore = 0;
-document.getElementById("playerScore").innerHTML=`Player: ` + playerScore;
-document.getElementById("computerScore").innerHTML=`Computer: `+ computerScore;
+document.getElementById("playerScore").innerHTML = `Player: ` + playerScore;
+document.getElementById("computerScore").innerHTML = `Computer: ` + computerScore;
 
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
-const choices = ['rock','paper','scissors']; 
+const choices = ['rock', 'paper', 'scissors'];
 
 let movesLeft = document.getElementsByClassName("movesLeft");
 movesLeft = 10;
-document.getElementsByClassName("movesLeft").innerHTML=`Moves Left: ` + movesLeft;
+document.getElementsByClassName("movesLeft").innerHTML = `Moves Left: ` + movesLeft;
 
 function game(player) {
     const computer = computerChoiceGen();
-    
-    if (player === 'rock' && computer === 'rock'){
+
+    if (player === 'rock' && computer === 'rock') {
         alert('Tie!');
         playerScore++;
         computerScore++;
-    } else if(player === 'rock' && computer === 'paper') {
+    } else if (player === 'rock' && computer === 'paper') {
         alert('Computer Wins!');
         computerScore++;
-    } else if (player === 'rock' && computer === 'scissors'){
+    } else if (player === 'rock' && computer === 'scissors') {
         alert('Player Wins!');
         playerScore++;
     }
-    if (player === 'paper' && computer === 'paper'){
+    if (player === 'paper' && computer === 'paper') {
         alert('Tie!');
         playerScore++;
         computerScore++;
-    } else if(player === 'paper' && computer === 'scissors'){
+    } else if (player === 'paper' && computer === 'scissors') {
         alert('Computer Wins!');
         computerScore++;
-    } else if (player === 'paper' && computer === 'rock'){
+    } else if (player === 'paper' && computer === 'rock') {
         alert('Player Wins!');
         playerScore++;
     }
-    if (player === 'scissors' && computer === 'scissors'){
+    if (player === 'scissors' && computer === 'scissors') {
         alert('Tie!');
         playerScore++;
         computerScore++;
-    } else if(player === 'scissors' && computer === 'rock'){
+    } else if (player === 'scissors' && computer === 'rock') {
         alert('Computer Wins!');
         computerScore++;
-    } else if (player === 'scissors' && computer === 'paper'){
+    } else if (player === 'scissors' && computer === 'paper') {
         alert('Player Wins!');
         computerScore++;
     }
     Gameover()
-    document.getElementById("playerScore").innerHTML=`Player: ` + playerScore;
-    document.getElementById("computerScore").innerHTML=`Computer: `+ computerScore;
+    document.getElementById("playerScore").innerHTML = `Player: ` + playerScore;
+    document.getElementById("computerScore").innerHTML = `Computer: ` + computerScore;
     console.log(computer);
     console.log(player);
     console.log(playerScore);
@@ -61,7 +60,7 @@ function game(player) {
 
 }
 
-function computerChoiceGen(){
+function computerChoiceGen() {
     const choiceNumber = Math.floor(Math.random() * choices.length)
     return choices[choiceNumber];
 }
@@ -69,35 +68,36 @@ function computerChoiceGen(){
 
 
 function buttonPick() {
-    rockBtn.addEventListener("click", function() {
+    rockBtn.addEventListener("click", function () {
         game("rock");
-     })
-
-     paperBtn.addEventListener("click", function() {
-        game("paper");
-     })
-    
-    scissorsBtn.addEventListener("click", function() {
-            game("scissors");
     })
-    
+
+    paperBtn.addEventListener("click", function () {
+        game("paper");
+    })
+
+    scissorsBtn.addEventListener("click", function () {
+        game("scissors");
+    })
+
 }
 
 buttonPick();
 
-function Gameover(){
+function Gameover() {
 
-if (playerScore === 10){
-    alert('Gameover - Player Wins!')
-    location.href = "/assets/html/playerwin.html"
-    resetScores();
-} else if (computerScore === 10){
-    alert('Gameover - Computer Wins!')
-    location.href = "/assets/html/computerwin.html"
-    resetScores();
+    if (playerScore === 10) {
+        alert('Gameover - Player Wins!')
+        location.href = "/assets/html/playerwin.html"
+        resetScores();
+    } else if (computerScore === 10) {
+        alert('Gameover - Computer Wins!')
+        location.href = "/assets/html/computerwin.html"
+        resetScores();
+    }
 }
-}
-function resetScores(){
+
+function resetScores() {
 
     playerScore = 0;
     computerScore = 0;
